8e4bf770e05026e4e84be75e76ef35573b31eb29
Aurélien <aurel.ruppe@gmail.com>
2024-05-22 19:01:48 +0200

    LevelOne

--- src/PokeSmart/TestTileGrid.java
+++ src/PokeSmart/TestTileGrid.java
@@ -1,7 +1,6 @@
 package PokeSmart;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -18,6 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class TestTileGrid extends Application {
     private final int TILE_SIZE = 48; // Taille d'une tuile en pixels
     private final int NUM_TILES_X = 16; // Nombre de tuiles en largeur
@@ -27,10 +27,18 @@ public class TestTileGrid extends Application {
     private boolean[][] collisionMap;
     private List<Item> items;
     private List<Entity> entities;
-
     private VBox inventoryBox;
 
-    private void initCaracters(){
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("PokeSmart - world1");
+        initCaractersWorld1();
+        String worldPath1 = "src/PokeSmart/Tiles/world1.csv";
+        GenMap(primaryStage, worldPath1, entities, items);
+    }
+
+    private void initCaractersWorld1(){
         player = new Player("Popo", 6, 1, 0, 0, 100, 0,100, 2, 100, 1000, 3, "src/PokeSmart/Player/Walking sprites/boy_down_1.png");
         entities = new ArrayList<Entity>();
         //entities.add(new Player("Popo", 6, 1, 0, 0, 100, 0,100, 2, 100, 1000, 3, "src/PokeSmart/Player/Walking sprites/boy_down_1.png"));
@@ -43,15 +51,9 @@ public class TestTileGrid extends Application {
         items.add(new Item(7,5,"Key", "this can open doors", Effet.OPENDOOR,1,"src/PokeSmart/Object/key.png"));
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("PokeSmart - world1");
-        initCaracters();
-        String worldPath1 = "src/PokeSmart/Tiles/world1.csv";
-        GenMap(primaryStage, worldPath1);
-    }
 
-    private void GenMap(Stage primaryStage, String wPath){
+
+    private void GenMap(Stage primaryStage, String wPath, List<Entity> entities, List<Item> items){
         String worldPath = wPath;
         // Chargement des données depuis le fichier CSV
         Image[][] tileImages = loadTileImages(worldPath);
@@ -59,22 +61,17 @@ public class TestTileGrid extends Application {
         // Création de la carte
         GridPane gridPane = createMap(tileImages);
 
-        // Création des éléments supplémentaires dans la VBox
-        VBox vBox = new VBox();
-        // Ajoutez vos éléments supplémentaires ici, par exemple :
-        // vBox.getChildren().add(new Button("Button"));
-        // Create inventory VBox
+        // Création de la boîte d'inventaire
         inventoryBox = new VBox();
         updateInventoryBox();
 
         // Création de la scène
-        //Group root = new Group();
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, NUM_TILES_X * TILE_SIZE + 200, NUM_TILES_Y * TILE_SIZE);
-        //root.getChildren().addAll(gridPane, vBox);
         root.setLeft(gridPane);
         root.setCenter(inventoryBox);
 
+        // Création de l'ImageView du joueur
         ImageView playerImageView = player.getImage();
         showEntities(playerImageView, root);
 
@@ -83,6 +80,72 @@ public class TestTileGrid extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+
+
+    private Image[][] loadTileImages(String filePath) {
+        Image[][] tileImages = new Image[NUM_TILES_X][NUM_TILES_Y];
+        collisionMap = new boolean[NUM_TILES_X][NUM_TILES_Y];
+        updateCollisionMap(tileImages, filePath);
+        return tileImages;
+    }
+
+
+
+    private GridPane createMap(Image[][] tileImages) {
+        GridPane gridPane = new GridPane();
+        for (int y = 0; y < NUM_TILES_Y; y++) {
+            for (int x = 0; x < NUM_TILES_X; x++) {
+                ImageView imageView = new ImageView(tileImages[x][y]);
+                imageView.setFitWidth(TILE_SIZE);
+                imageView.setFitHeight(TILE_SIZE);
+                gridPane.add(imageView, x, y);
+            }
+        }
+        return gridPane;
+    }
+
+
+
+    private void updateInventoryBox() {
+        inventoryBox.getChildren().clear();
+        Label inventoryLabel = new Label(player.getInventoryAsString());
+        inventoryBox.getChildren().add(inventoryLabel);
+    }
+
+
+
+    private void showEntities(ImageView playerImageView, BorderPane root) {
+        // player
+        playerImageView.setFitWidth(TILE_SIZE); // Ajustez la taille de l'image selon vos besoins
+        playerImageView.setFitHeight(TILE_SIZE); // Ajustez la taille de l'image selon vos besoins
+        root.getChildren().add(playerImageView); // Ajout de l'ImageView du joueur à la scène
+
+        playerImageView.setLayoutX(player.getX() * TILE_SIZE);
+        playerImageView.setLayoutY(player.getY() * TILE_SIZE);
+
+        // entities
+        for (Entity entity : entities) {
+            ImageView entityImageView = entity.getImage();
+            entityImageView.setFitWidth(TILE_SIZE);
+            entityImageView.setFitHeight(TILE_SIZE);
+            root.getChildren().add(entityImageView);
+            entityImageView.setLayoutX(entity.getX() * TILE_SIZE);
+            entityImageView.setLayoutY(entity.getY() * TILE_SIZE);
+        }
+
+        // items
+        for (Item item : items) {
+            ImageView itemImageView = item.getImage();
+            itemImageView.setFitWidth(TILE_SIZE);
+            itemImageView.setFitHeight(TILE_SIZE);
+            root.getChildren().add(itemImageView);
+            itemImageView.setLayoutX(item.getX() * TILE_SIZE);
+            itemImageView.setLayoutY(item.getY() * TILE_SIZE);
+        }
+    }
+
+
 
     private void keySet(Scene scene, ImageView playerImageView, BorderPane root, Image[][] tileImages, String worldPath, Stage primaryStage) {
         scene.setOnKeyPressed(e -> {
@@ -126,46 +189,16 @@ public class TestTileGrid extends Application {
 
             checkForItemPickup(root, tileImages, worldPath);
 
-            // vérifier si le joueur est sur la porte
+            // vérifier si le joueur est sur la porte du monde 1
             if (player.getX() == 14 && player.getY() == 10) {
                 System.out.println("You win!");
-                //primaryStage.close();
+                primaryStage.close();
                 createNewWorld();
             }
         });
     }
 
-    private void createNewWorld() {
-        // Par exemple, vous pouvez appeler la méthode GenMap avec un nouveau fichier de carte
-        String newWorldPath = "src/PokeSmart/Tiles/world2.csv";
-        Stage newWorldStage = new Stage();
-        newWorldStage.setTitle("PokeSmart - world2");
-        GenMap(newWorldStage, newWorldPath);
-    }
 
-    private void checkForItemPickup(BorderPane root, Image[][] tileImages, String filePath) {
-        List<Item> pickedUpItems = new ArrayList<>();
-        for (Item item : items) {
-            if (player.getX() == item.getX() && player.getY() == item.getY()) {
-                player.addItem(item);
-                pickedUpItems.add(item);
-                System.out.println("Item picked up");
-                root.getChildren().remove(item.getImage());
-                updateInventoryBox();
-                item.useItem(player);
-                System.out.println(player.getCapacities());
-            }
-        }
-        items.removeAll(pickedUpItems);
-
-        updateCollisionMap(tileImages, filePath); // Mettre à jour la carte des collisions
-    }
-
-    private void updateInventoryBox() {
-        inventoryBox.getChildren().clear();
-        Label inventoryLabel = new Label(player.getInventoryAsString());
-        inventoryBox.getChildren().add(inventoryLabel);
-    }
 
     private void updateCollisionMap(Image[][] tileImages, String filePath) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -197,12 +230,49 @@ public class TestTileGrid extends Application {
     }
 
 
-    private Image[][] loadTileImages(String filePath) {
-        Image[][] tileImages = new Image[NUM_TILES_X][NUM_TILES_Y];
-        collisionMap = new boolean[NUM_TILES_X][NUM_TILES_Y];
-        updateCollisionMap(tileImages, filePath);
-        return tileImages;
+
+    private void checkForItemPickup(BorderPane root, Image[][] tileImages, String filePath) {
+        List<Item> pickedUpItems = new ArrayList<>();
+        for (Item item : items) {
+            if (player.getX() == item.getX() && player.getY() == item.getY()) {
+                player.addItem(item);
+                pickedUpItems.add(item);
+                System.out.println("Item picked up");
+                root.getChildren().remove(item.getImage());
+                updateInventoryBox();
+                item.useItem(player);
+                System.out.println(player.getCapacities());
+            }
+        }
+        items.removeAll(pickedUpItems);
+
+        updateCollisionMap(tileImages, filePath); // Mettre à jour la carte des collisions
     }
+
+
+
+    private void createNewWorld() {
+        // Par exemple, vous pouvez appeler la méthode GenMap avec un nouveau fichier de carte
+        String newWorldPath = "src/PokeSmart/Tiles/world2.csv";
+        Stage newWorldStage = new Stage();
+        newWorldStage.setTitle("PokeSmart - world2");
+        initCaractersWorld2();
+        GenMap(newWorldStage, newWorldPath, entities, items);
+    }
+
+
+
+    private void initCaractersWorld2(){
+        entities = new ArrayList<Entity>();
+        //entities.add(new Player("Popo", 6, 1, 0, 0, 100, 0,100, 2, 100, 1000, 3, "src/PokeSmart/Player/Walking sprites/boy_down_1.png"));
+        entities.add(new Monster("Papa", 7, 2, 0, 0, 1,0, "OFFENSIVE", 1, 1, 1, 1,"src/PokeSmart/Monster/orc_down_2.png"));
+
+        items = new ArrayList<Item>();
+        items.add(new Item(7,3,"HealPotion", "this can heal you", Effet.HEAL,1,"src/PokeSmart/Object/potion_red.png"));
+        items.add(new Item(7,4,"WallPotion", "no more walls", Effet.OVERWALL,1,"src/PokeSmart/Object/potion_blue.png"));
+    }
+
+
 
     private String getImagePathForValue(int value) {
         switch (value) {
@@ -224,48 +294,7 @@ public class TestTileGrid extends Application {
         }
     }
 
-    private GridPane createMap(Image[][] tileImages) {
-        GridPane gridPane = new GridPane();
-        for (int y = 0; y < NUM_TILES_Y; y++) {
-            for (int x = 0; x < NUM_TILES_X; x++) {
-                ImageView imageView = new ImageView(tileImages[x][y]);
-                imageView.setFitWidth(TILE_SIZE);
-                imageView.setFitHeight(TILE_SIZE);
-                gridPane.add(imageView, x, y);
-            }
-        }
-        return gridPane;
-    }
 
-    private void showEntities(ImageView playerImageView, BorderPane root) {
-        // player
-        playerImageView.setFitWidth(TILE_SIZE); // Ajustez la taille de l'image selon vos besoins
-        playerImageView.setFitHeight(TILE_SIZE); // Ajustez la taille de l'image selon vos besoins
-        root.getChildren().add(playerImageView); // Ajout de l'ImageView du joueur à la scène
-
-        playerImageView.setLayoutX(player.getX() * TILE_SIZE);
-        playerImageView.setLayoutY(player.getY() * TILE_SIZE);
-
-        // entities
-        for (Entity entity : entities) {
-            ImageView entityImageView = entity.getImage();
-            entityImageView.setFitWidth(TILE_SIZE);
-            entityImageView.setFitHeight(TILE_SIZE);
-            root.getChildren().add(entityImageView);
-            entityImageView.setLayoutX(entity.getX() * TILE_SIZE);
-            entityImageView.setLayoutY(entity.getY() * TILE_SIZE);
-        }
-
-        // items
-        for (Item item : items) {
-            ImageView itemImageView = item.getImage();
-            itemImageView.setFitWidth(TILE_SIZE);
-            itemImageView.setFitHeight(TILE_SIZE);
-            root.getChildren().add(itemImageView);
-            itemImageView.setLayoutX(item.getX() * TILE_SIZE);
-            itemImageView.setLayoutY(item.getY() * TILE_SIZE);
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
